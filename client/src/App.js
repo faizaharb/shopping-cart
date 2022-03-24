a4a4edd677e321bdf9996a86a49290cce7a61179
faizaharb <fandshassan@gmail.com>
2022-03-25 00:18:58 +0200

    header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "./Components/Header/Header";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="layOut">
+        <Header />
+        <main>main</main>
+        <footer>
+          <h4>footer</h4>
+        </footer>
+      </div>
+    </>
+  );
 }
